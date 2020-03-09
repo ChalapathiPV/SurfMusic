ffdba7e0e5d031221429c01b2105a15fe1fbf7e8
ChalapathiPV <chalapathi.dev@gmail.com>
2020-03-09 06:14:41 +0530

    Project setup

---
+++ SurfMusic/View Controllers/Root View Controller/RootViewController.swift
@@ -0,0 +1,21 @@
+//
+//  RootViewController.swift
+//  SurfMusic
+//
+//  Created by PVC on 08/03/20.
+//  Copyright © 2020 Chalapathi. All rights reserved.
+//
+
+import UIKit
+
+final class RootViewController: UIViewController {
+
+    // MARK: - View Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
