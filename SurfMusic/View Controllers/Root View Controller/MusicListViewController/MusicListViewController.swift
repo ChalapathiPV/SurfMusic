77dc45f3ae190dad685c0e04da08db3ea4b5f75c
Chalapathi PV <chalapathi.dev@gmail.com>
2020-03-09 10:11:35 +0530

    Add Music list view controller

---
+++ SurfMusic/View Controllers/Root View Controller/MusicListViewController/MusicListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MusicListViewController.swift
+//  SurfMusic
+//
+//  Created by PVC on 09/03/20.
+//  Copyright © 2020 Chalapathi. All rights reserved.
+//
+
+import UIKit
+
+final class MusicListViewController: UIViewController {
+
+    // Mark: - View Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Setup view
+        setUpView()
+    }
+    
+    // Mark: - Helper Methods
+    
+    private func setUpView() {
+        // Configure View
+        
+    }
+
+}
